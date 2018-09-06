aa1cc2054e4a0a747d3ca6defd1c9cbdbe4e2b2c
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-06 20:46:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,18 @@
-def countdown_with_sleep(integer)
-  while integer > 0
-    puts "#{integer} SECOND(S)!"
-    sleep 1
-    integer -= 1
-    return "HAPPY NEW YEAR!"
-  end
-end
+
 
 
 def countdown(integer)
   while integer > 0
     puts "#{integer} SECOND(S)!"
+    integer -= 1
+    return "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_with_sleep(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    sleep 1
     integer -= 1
     return "HAPPY NEW YEAR!"
   end
